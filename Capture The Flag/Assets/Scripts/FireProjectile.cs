78b3450e6810b45f22e5d62b6e7ce3c23ecf466f
CoderJonah <jonahwr@verizon.net>
2020-03-26 13:38:00 -0400

    Updated shoot script, deleted unnecessary components, commented code

---
+++ Capture The Flag/Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class FireProjectile : MonoBehaviour
+{
+
+    [SerializeField] private Transform projectileTransform;
+    [SerializeField] private float projectileFireSpeed = 30f;
+    [SerializeField] private float minimumSecondsTilProjectileFire = 1.0f;
+
+    private float secondsTilProjectileFire = 0f;
+    
+    //input variables
+    private Mouse mouse;
+    private Gamepad gamepad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get input on start
+        mouse = Mouse.current;
+        //if mouse not connected
+        if( mouse == null )
+        {
+            Debug.Log("No mouse connected");
+        }
+        else {
+            print("Mouse connected");
+        }
+        gamepad = Gamepad.current;
+        //if gamepad not connected
+        if( gamepad == null )
+        {
+            Debug.Log("No gamepad connected");
+        }
+        else
+        {
+            print("Gamepad connected");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update number of seconds til next shot
+        secondsTilProjectileFire -= Time.deltaTime;
+        //if mouse connected
+        if( mouse != null )
+        {   //if left click and projectile ready
+            if(mouse.leftButton.isPressed && secondsTilProjectileFire <= 0f )
+            {
+                //instantiate projectile, reset timer
+                secondsTilProjectileFire = ShootProjectile(secondsTilProjectileFire);
+            }
+        }
+        //otherwise try to find the mouse
+        else {
+            mouse = Mouse.current;
+            //if mouse connected
+            if( mouse != null )
+            {
+                print("Mouse connected");
+            }
+        }
+        //if gamepad connected
+        if(gamepad != null){
+            //if right trigger pressed and projectile ready
+            if( gamepad.rightShoulder.isPressed && secondsTilProjectileFire <= 0f){
+                //instantiate projectile, reset timer
+                secondsTilProjectileFire = ShootProjectile(secondsTilProjectileFire);
+            }
+
+        }
+        //otherwise try to find gamepad
+        else{
+            gamepad = Gamepad.current;
+            if( gamepad != null )
+            {
+                print("Gamepad connected");
+            }
+
+        }
+    }
+
+
+    //instantiates a projectile and sets it's velocity
+    private float ShootProjectile(float secondsTilProjectileFire)
+    {
+        //reset timer
+        secondsTilProjectileFire = minimumSecondsTilProjectileFire;
+        //instantiate projectile
+        Transform firedProjectileClone = Object.Instantiate(projectileTransform, this.transform.position, this.transform.rotation);
+        //get each child of projectile
+        foreach( Transform child in firedProjectileClone.transform )
+        {
+            //get projectile rigidbody
+            Rigidbody  projectileRb = child.GetComponent<Rigidbody>();
+            if( projectileRb != null ) {
+                //set projectile velocity
+                projectileRb.velocity = this.transform.forward * projectileFireSpeed;
+                //stop searching for rigidbody
+                break;
+            }
+        }
+        //return timer
+        return secondsTilProjectileFire;
+    }
+}
--- Capture The Flag/Assets/Scripts/ThirdPersonCamera.cs
+++ Capture The Flag/Assets/Scripts/ThirdPersonCamera.cs
@@ -6,92 +6,92 @@ using UnityEngine.InputSystem.Controls;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform lookAt;
+    //Location of camera
     public Transform camTransform;
-
-    [SerializeField]
-    private Camera cam;
-
-    Mouse mouse;
+    //player location
+    public Transform lookAt;
+    //input
+    Mouse mouse = null;
     Gamepad gamepad = null;
-    UnityEngine.InputSystem.Utilities.ReadOnlyArray<UnityEngine.InputSystem.Gamepad> gamepads;
-
-    //private Vector2 lookInput;
-    //private CameraControls input;
-
-    private const float Y_ANGLE_MIN = 0.0f;
-    private const float Y_ANGLE_MAX = 60.0f;
-
-    public float camDistance;
+    //camera angle bounds
+    [SerializeField] private const float Y_ANGLE_MIN = 0.0f;
+    [SerializeField] private const float Y_ANGLE_MAX = 60.0f;
+    //distance from camera to player
+    [SerializeField] private float camDistance = 0f;
+    //current camera X angle
     private float currX = 0.0f;
+    //current camera Y angle
     private float currY = 0.0f;
-    public float deadzoneX = 0.05f;
-    public float deadzoneY = 0.05f;
-
-    public float sensitivityX = 1.0f;
-    public float sensitivityY = 1.0f;
-
-    public float sensitivityXGamepadRightStick = 100f;
-    public float sensitivityYGamepadRightStick = 100f;
-    public float deadzoneXGamepadRightStick = 0.05f;
-    public float deadzoneYGamepadRightStick = 0.05f;
+    [SerializeField] public float deadzoneX = 0.05f;
+    [SerializeField] public float deadzoneY = 0.05f;
+    //mouse sensitivity
+    [SerializeField] public float sensitivityX = 1.0f;
+    [SerializeField] public float sensitivityY = 1.0f;
+    //gamepad parameters
+    [SerializeField] public float sensitivityXGamepadRightStick = 100f;
+    [SerializeField] public float sensitivityYGamepadRightStick = 100f;
+    [SerializeField] public float deadzoneXGamepadRightStick = 0.05f;
+    [SerializeField] public float deadzoneYGamepadRightStick = 0.05f;
 
     private void Awake()
-    {
+    {   //get input mechanisms
         mouse = Mouse.current;
         gamepad = Gamepad.current;
-        gamepads = Gamepad.all;
     }
 
-        private void Start()
+    private void Start()
     {
-        camDistance = cam.transform.position.z - lookAt.position.z;
-        
-
+        //calculate distance of  camera to player
+        camDistance = this.transform.position.z - lookAt.position.z;
     }
 
     private void Update()
     {
+        //get mouse delta
         float mouseX = mouse.delta.x.ReadValue();
         float mouseY = mouse.delta.y.ReadValue();
-        float gamepadRightStickX = 0f;
-        float gamepadRightStickY = 0f;
-        bool lookingX = false;
-        bool lookingY = false;
-        if (gamepad != null)
+        //if no mouse input and gamepad is connected
+        if (mouseX == 0 && mouseY == 0 && gamepad != null)
         {
-            gamepadRightStickX = gamepad.rightStick.ReadValue().x;
-            gamepadRightStickY = gamepad.rightStick.ReadValue().y;
-            lookingX = (Mathf.Abs(gamepadRightStickX) > deadzoneXGamepadRightStick);
-            lookingY = (Mathf.Abs(gamepadRightStickY) > deadzoneYGamepadRightStick);
+            //get gamepad right stick position
+            Vector2 gamepadRightStickPosition = getGamepadRightStickPosition(gamepad);
+
+            currX += gamepadRightStickPosition.x * sensitivityXGamepadRightStick * Time.deltaTime;
+            currY -= gamepadRightStickPosition.y * sensitivityYGamepadRightStick * Time.deltaTime;
         }
-        if (mouseX == 0 && mouseY == 0 && (lookingX || lookingY))
-        {
-            if (lookingX)
-            {
-                currX += gamepadRightStickX * sensitivityXGamepadRightStick * Time.deltaTime;
-            }
-            if (lookingY)
-            {
-                currY -= gamepadRightStickY * sensitivityYGamepadRightStick * Time.deltaTime;
-            }
-        }
-        
+        //if mouse input or gamepad not connected
         else
         {
             currX += mouseX * sensitivityX * Time.deltaTime;
             currY -= mouseY * sensitivityY * Time.deltaTime;
-
         }
+        //clamp camera Y angle
         currY = Mathf.Clamp(currY, Y_ANGLE_MIN, Y_ANGLE_MAX);
     }
 
     private void LateUpdate()
     {
+        //point camera at character
         Vector3 dir = new Vector3(0, 0, camDistance);
-
         Quaternion rotation = Quaternion.Euler(currY, currX, 0);
-        camTransform.position = lookAt.position + rotation * dir;
-        camTransform.LookAt(lookAt.position);
+        this.transform.position = lookAt.position + rotation * dir;
+        this.transform.LookAt(lookAt.position);
+    }
+
+    private Vector2 getGamepadRightStickPosition(Gamepad gamepad) {
+        //get right stick input
+        float gamepadRightStickX = gamepad.rightStick.ReadValue().x;
+        float gamepadRightStickY = gamepad.rightStick.ReadValue().y;
+        
+        //check if less than deadzone
+        if( Mathf.Abs(gamepadRightStickX) < deadzoneXGamepadRightStick )
+        {
+            gamepadRightStickX = 0f;
+        }
+        if( Mathf.Abs(gamepadRightStickY) < deadzoneYGamepadRightStick )
+        {
+            gamepadRightStickY = 0f;
+        }
+        return new Vector2(gamepadRightStickX, gamepadRightStickY);
     }
 }
